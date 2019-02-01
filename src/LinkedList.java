9bdef82ea92b0d4df91e2e9351a9d1a3d04850a2
Munchies012 <weeniehutjr0145@gmail.com>
2019-02-01 07:58:34 -1000

    Initial commit

---
+++ src/LinkedList.java
@@ -0,0 +1,14 @@
+
+public final class LinkedList{
+
+
+
+
+    public final static void main(String[] args){
+        int num = 4562;
+        new StringBuilder("4562").reverse().toString();
+        System.out.println(new StringBuilder("4562").reverse().toString());
+
+
+}
+}
